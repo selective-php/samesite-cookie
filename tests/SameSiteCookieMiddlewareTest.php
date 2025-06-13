a001fcd8f7fd114880b2ddfee6f3ad69c0dc2a13
heinrichschiller <heinrich@heinrich-schiller.de>
2025-06-13 13:13:35 +0200

    test with settings

--- tests/SameSiteCookieMiddlewareTest.php
+++ tests/SameSiteCookieMiddlewareTest.php
@@ -32,4 +32,29 @@ class SameSiteCookieMiddlewareTest extends TestCase
         $this->assertSame('PHPSESSID=v3absd19o9pi6cjvhb5pkmsfo9; path=/; Secure; HttpOnly; SameSite=Lax;', $cookie);
         $this->assertSame('', (string)$response->getBody());
     }
+
+    /**
+     * Test with own settings.
+     */
+    public function testDefaultConfigurationWithOwnSettings(): void
+    {
+        $settings = [
+            'start_session' => true,
+            'same_site' => 'Strict',
+            'http_only' => false,
+        ];
+
+        $configuration = new SameSiteCookieConfiguration($settings);
+
+        session_id('v3absd19o9pi6cjvhb5pkmsfo9');
+
+        $response = $this->runQueue([
+            new SameSiteSessionMiddleware(),
+            new SameSiteCookieMiddleware($configuration),
+        ]);
+
+        $cookie = $response->getHeaderLine('Set-Cookie');
+        $this->assertSame('PHPSESSID=v3absd19o9pi6cjvhb5pkmsfo9; path=/; Secure; SameSite=Strict;', $cookie);
+        $this->assertSame('', (string)$response->getBody());
+    }
 }
